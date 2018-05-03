543d762bc875f6ad2e07098d0f2e2a6af48a0cc5
Angus Chiu <anguschiu@gmail.com>
2018-05-03 16:19:08 +0800

    minor change for lintings

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
+Vue.config.devtools = true
 Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
